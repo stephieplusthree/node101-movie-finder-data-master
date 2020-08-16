1d9e1aad2f86e77a4174953f323d3a7e078aaf79
stephanie <stephieplusthree@gmail.com>
2020-08-15 18:47:40 -0700

    detail css

--- client/src/js/components/ContentRow.js
+++ client/src/js/components/ContentRow.js
@@ -2,7 +2,6 @@
 import React, { useState, useCallback } from 'react';
 import { css, jsx } from '@emotion/core';
 import styled from '@emotion/styled';
-import ContentBlock from './ContentBlock';
 
 import one from '../../img/one.jpg';
 import two from '../../img/two.jpg';
@@ -53,7 +52,9 @@ const ContentRow = ({ category, setActive }) => {
 
         <div className="block-wrapper">
           {content.map(img => (
-            <ContentBlock key={img} img={img}
+            <ContentBlock 
+              key={img} 
+              data-img={img}
               onMouseEnter={handleHover}
               onMouseLeave={handleHover}
             >
@@ -72,5 +73,96 @@ const ContentRow = ({ category, setActive }) => {
     </div>
   )
 }
+
+const ContentBlock = styled.div`
+    position: relative; 
+    /* puts space in between movie cards */
+    flex: calc(18vw - 4px);
+    flex-shrink: 0;
+    height: 9.5vw;
+    margin-right: 4px;
+    display: flex;
+    img {
+      height: 100%;
+      width: 100%;
+      display: flex;
+      pointer-events: none; // Very important!
+    }
+    h2 {
+        margin: 20px 0 10px;
+        color: white;
+    }
+    .block-wrapper {
+        display: flex;
+        width: 100%;
+        position: relative;
+    }
+    .content {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    transition: background-color ease 0.2s;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.65);
+      cursor: pointer;
+    }
+    .Icon {
+      font-size: 32px;
+      color: red;
+    }
+    
+  `
+
+const style = ({
+    contentRow: {
+        paddingLeft: "60px",
+        overflowX: "hidden",
+        display: "flex",
+
+    },
+    genre: {
+        margin: "20px 0 10px",
+        color: "white",
+    },
+    blockWrapper: {
+        display: "flex",
+        width: "100%",
+        position: "relative",
+    },
+    contentBlock: {
+        position: "relative",
+        flex: "calc(18vw - 4px)",
+        height: "9.5vw",
+        marginRight: "4px",
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        pointerEvents: "none",
+    },
+    content: {
+        position: "absolute",
+        display: "flex",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        justifyContent: "center",
+        transition: "backgrounColor ease 0.2s",
+    },
+    iconplay: {
+        fontSize: "25px",
+        color: "red",
+        marginRight: "20px",
+        marginBottom: "25px"
+    },
+    icon: {
+        fontSize: "25px",
+        color: "white",
+        marginRight: "20px"
+    },
+})
 
 export default ContentRow;
